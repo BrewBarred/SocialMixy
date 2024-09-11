18425e24e7b743de64eac6f24cb6ff35b8a36b72
Eli <elayjahh7@hotmail.com>
2024-09-11 22:42:43 +1200

    Add project files.

---
+++ Social Mixy/Main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Social_Mixy
+{
+    public partial class Main : Form
+    {
+        // class scope variables
+
+        // stores a list of everyone that has pre-registered to the social mixy
+        List<Person> listRegistrants = new List<Person>();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The main method that is executed on load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // creates event handlers for when a file is dragged and dropped onto the form
+            this.DragEnter += new DragEventHandler(FormDragEnter);
+            this.DragDrop += new DragEventHandler(FormDragDrop);
+        }
+
+        public void FormDragEnter(object sender, DragEventArgs e)
+        {
+            // check if the dragged item is a file
+            e.Effect = (e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None);
+        }
+
+        public void FormDragDrop(object sender, DragEventArgs e)
+        {
+            // fetch the file path of the dropped file
+            string filePath = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+
+            // ensures the dropped file is a valid .csv file
+            if (Path.GetExtension(filePath).ToLower() == ".csv")
+                processFile(filePath);
+            else
+                MessageBox.Show("Invalid file extension detected! Please ensure the dropped file is of \".csv\" format!");
+        }
+
+        public void processFile(string filePath)
+        {
+            // stores the number of registrants
+            int registerId = 0;
+
+            // creates a stream reader to use which is automatically disposed of afterwards
+            using (var reader = new StreamReader(filePath))
+            {
+                // reads the first line to exclue the headings from the csv file
+                reader.ReadLine();
+
+                // loop through the data until the end of the stream is reached
+                while (!reader.EndOfStream)
+                {
+                    // splits each line of data by comma
+                    string[] line = reader.ReadLine().Split(new string[] { "\",\"" }, StringSplitOptions.None);
+
+                    // check that the line length is valid
+                    if (line.Length > 1)
+                    {
+                        // increment registerId to track how many people have registered in what order
+                        registerId++;
+                        // create a new person object to store in the list of registered people
+                        Person p = new Person(line, registerId);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Social Mixy/Person.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+
+namespace Social_Mixy
+{
+    public class Person
+    {
+        // the registration number of this person (1 = first person to register, 2 = second...)
+        public int numRegistration;
+        // the date that this person registered for the event
+        public string date;
+        // the campus that this person typically attends
+        public string campus;
+        // true if this person requires a ride from TGA to Ham and back, else false
+        public bool isRideRequiredHamilton;
+        // true if this person requires a ride home from TGA, else false
+        public bool isRideRequiredHome;
+        // true if this person is willing to carpool other students, else false
+        public bool isCarpoolFriendly;
+        // true if this person has accepted our disclaimer notice, else false
+        public bool isAcceptingDisclaimer;
+        // the number of passengers that this person can carpool
+        public string passengerLimit;
+        // the association between this person and the university (i.e., Student, Staff or Alumni)
+        public string associationType;
+        // the first name of this person
+        public string firstName;
+        // the last name of this person
+        public string lastName;
+        // the student id of this person
+        public int studentId;
+        // the level of study of this person (i.e., 1st year, 2nd year, masters etc.,)
+        string studyLevel;
+        // the first major of this person
+        public string firstMajor;
+        // the email address of this person
+        public string email;
+        // the field of psychology that this person specializes in (staff and alumni only)
+        public string specialization;
+        // true if this person would like to volunteer to be a panellist, else false
+        public bool isVolunteering;
+        // true if this person has any dietary preferences, else false
+        public bool hasDietaryPrefs;
+        // a string denoting the dietary preferences of this person
+        public string dietaryPrefs;
+        // true if this person would like to receive an update via email
+        public bool isEmailable;
+        // extra notes provided by this person
+        public string notes;
+
+        public Person(string[] data, int id)
+        {
+            numRegistration = id;
+            setAttributes(data);
+            // test print
+            printAttributes();
+        }
+        private void setAttributes(string[] data)
+        {
+            // sets this persons attributes based on the information passed on construction
+            date = data[0].Substring(1, 11).ToString();
+            campus = data[1];
+            isRideRequiredHamilton = data[2].Contains("Yes");
+            isRideRequiredHome = data[3].Contains("Yes");
+            isCarpoolFriendly = data[4].Contains("Yeah");
+            isAcceptingDisclaimer = data[5].Contains("Yeah");
+            passengerLimit = data[6] == "" ? "N/A" : data[6];
+            firstName = data[8] == "" ? (data[14] == "" ? data[20] : data[14]) : data[8];
+            lastName = data[9] == "" ? (data[15] == "" ? data[21] : data[15]) : data[9];
+            studentId = data[10] != "" ? int.Parse(data[10]) : -1;
+            studyLevel = data[11] == "" ? null : data[11];
+            firstMajor = data[12] == "" ? (data[16] == "" ? null : data[16]) : data[12];
+            email = data[13] == "" ? (data[17] == "" ? data[23] : data[17]) : data[13];
+            specialization = data[18] == "" ? (data[23] == "" ? null : data[23]) : data[18];
+            isVolunteering = data[19] == "No" ? (data[24] == "No" ? false : true) : true;
+            hasDietaryPrefs = data[25] == "Yes" ? true : false;
+            dietaryPrefs = hasDietaryPrefs == false ? "N/A" : data[26];
+            isEmailable = data[27].Contains("Yes");
+            notes = data[28];
+        }
+
+        public void printAttributes()
+        {
+            StringBuilder str = new StringBuilder(numRegistration.ToString() + "\n");
+            str.AppendLine("Registration Date: " + date);
+            str.AppendLine("Campus: " + campus);
+            str.AppendLine("Shuttle required? " + isRideRequiredHamilton);
+            str.AppendLine("Ride home required? " + isRideRequiredHome);
+            str.AppendLine("Carpool Friendly? " + isCarpoolFriendly);
+            str.AppendLine("Approved disclaimer? " + isAcceptingDisclaimer);
+            str.AppendLine("Passenger limit: " + passengerLimit);
+            str.AppendLine("First Name: " + firstName);
+            str.AppendLine("Last Name: " + lastName);
+            str.AppendLine("Student Id: " + (studentId == -1 ? "N/A" : studentId.ToString()));
+            str.AppendLine("Level of study: " + studyLevel);
+            str.AppendLine("First Major: " + firstMajor);
+            str.AppendLine("Email: " + email);
+            str.Append("");
+
+            Console.WriteLine(str);
+        }
+
+    }
+}
